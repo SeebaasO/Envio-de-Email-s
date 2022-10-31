2ff207753a019fd1c0a7b2199c3eb5c18014a85b
Sebas <sebas.09.pc@gmail.com>
2022-10-30 22:26:38 -0500

    Commit final

--- js/app.js
+++ js/app.js
@@ -1,39 +1,130 @@
 document.addEventListener('DOMContentLoaded', function(){
+
+    const email = {
+        email: '',
+        asunto: '',
+        mensaje: ''
+    }
+
     const inputEmail = document.querySelector('#email');
+    const inputCC = document.querySelector('#CC');
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
     const formulario = document.querySelector('#formulario');
+    const btnSubmit = document.querySelector('#formulario button[type="submit"]');
+    const btnReset = document.querySelector('#formulario button[type="reset"]');
+    const spiner = document.querySelector('#spiner');
     
-    inputEmail.addEventListener('blur', validar);
+    inputEmail.addEventListener('input', validar);
+    inputCC.addEventListener('input', validar)
+    inputAsunto.addEventListener('input', validar);
+    inputMensaje.addEventListener('input', validar);
 
-    inputAsunto.addEventListener('blur', validar);
+    formulario.addEventListener('submit', enviarEmail);
 
-    inputMensaje.addEventListener('blur', validar);
+    btnReset.addEventListener ('click', function(e){
+        e.preventDefault();     
+        resetFormulario();
+    })
 
+    function enviarEmail(e){
+        e.preventDefault();
+
+        spiner.classList.add('flex');
+        spiner.classList.remove('hidden');
+
+        setTimeout(() => {
+            spiner.classList.remove('flex');
+            spiner.classList.add('hidden');
+            resetFormulario();
+
+            const alertaExito = document.createElement('P');
+            alertaExito.classList.add('bg-green-500', 'text-white', 'p-2', 'text-center', 'rounded-lg', 'mt-10',
+            'font-bold', 'text-sm', 'uppercase');
+            alertaExito.textContent = 'Email enviado correctamente';
+
+            formulario.appendChild(alertaExito);
+            
+            setTimeout(() => {
+                alertaExito.remove();
+            }, 3000);
+
+        }, 2000);
+
+        
+    }
 
     function validar(e){
-        if(e.target.value.trim() === '')
+        if(e.target.id == 'CC' && !validarEmail(e.target.value)){
+            mostrarMensaje('Correo invalido', e.target.parentElement);
+            email[e.target.name] = '';
+            comprobarEmail();
+            return;
+        }
+
+        if(e.target.value.trim() === ''){
         mostrarMensaje(`El campo ${e.target.id} es obligatorio`, e.target.parentElement);
-        else
-            console.log('con texto');  
+        email[e.target.name] = '';
+        comprobarEmail();
+        return;
+        }   
+        
+        if(e.target.id == 'email' && !validarEmail (e.target.value)){
+            mostrarMensaje('Correo invalido', e.target.parentElement);
+            email[e.target.name] = '';
+            comprobarEmail();
+            return;
+        }
+           
+        limpiarAlerta(e.target.parentElement);
+
+        email[e.target.name] = e.target.value.trim().toLowerCase();
+
+        comprobarEmail();        
     }
     
     function mostrarMensaje(mensaje, referencia){
-
-        const alerta = referencia.querySelector('.alerta');
-
-        if(alerta){
-            alerta.remove();
-        }
+        limpiarAlerta(referencia);
         
         const error = document.createElement('P');
         error.textContent = mensaje;
         error.classList.add('p-2', 'text-center', 'alerta');
 
         referencia.appendChild(error);
-
         
+    }
 
+    function limpiarAlerta (referencia){
+        const alerta = referencia.querySelector('.alerta');
+        
+        if(alerta){
+            alerta.remove();
+        }
+    }
+
+    function validarEmail(email){
+        const regex =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/ 
+        const resultado = regex.test(email);
+        return resultado;
+    }
+
+    function comprobarEmail(){
+        if(!Object.values(email).includes('')){
+            btnSubmit.classList.remove('opacity-50');
+            btnSubmit.removeAttribute('disabled');
+        }else{
+            btnSubmit.classList.add('opacity-50');
+            btnSubmit.setAttribute('disabled', '');
+        }
+    }
+
+    function resetFormulario(){
+        formulario.reset();
+        email.email = '';
+        email.asunto = '';
+        email.mensaje = '';
+
+        comprobarEmail();
     }
 
 });
